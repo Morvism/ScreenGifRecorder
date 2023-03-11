a4d568ee657e6be3f3c2383689021af2ec5bc75c
Morteza Taghavi <98399498+Morvism@users.noreply.github.com>
2023-03-11 12:00:57 -0800

    Update Rec.py

--- Rec.py
+++ Rec.py
@@ -1,13 +1,17 @@
 import pyautogui
 import numpy as np
 import imageio
+import easygui
+
 
 fps = 10
 time = input("Time of gif  (to second) :")
 time = int(time)
 screen_size = pyautogui.size()
-nameFile = input("Name of file : ")
-output_file = nameFile +".gif"
+output_folder = easygui.diropenbox()
+output_file_name = easygui.enterbox("Enter the output file name")
+
+output_file = output_folder  + "/" + output_file_name + ".gif"
 
 with imageio.get_writer(output_file, mode='I', fps=fps) as writer:
     for i in range(fps * time):
